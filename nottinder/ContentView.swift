ed6340a143e3b3f154c3aa5139c669b3446a4d76
Reza Babaei <rzbabaei1@gmail.com>
2022-10-08 07:49:00 +0330

    Initial Commit

---
+++ nottinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nottinder
+//
+//  Created by Reza Babaei on 7/16/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nottinder/nottinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  nottinderApp.swift
+//  nottinder
+//
+//  Created by Reza Babaei on 7/16/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct nottinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
